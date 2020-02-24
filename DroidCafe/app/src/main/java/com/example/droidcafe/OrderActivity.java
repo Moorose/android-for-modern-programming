316f1e8be6fcd6e7fc41d052b31582a586050938
Eugene <ugrum98@gmail.com>
2020-02-24 12:03:22 +0600

    Add several fields for user

--- DroidCafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ DroidCafe/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -18,5 +21,27 @@ public class OrderActivity extends AppCompatActivity {
         if (message == null) message="Your order is empty";
         TextView textView = findViewById(R.id.order_textview);
         textView.setText("Order: " + message);
+    }
+
+    public void onRadioButtonClick(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        if(!checked) return;
+        switch (view.getId()){
+            case R.id.sameday:
+                displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                displayToast(getString(R.string.pick_up));
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
